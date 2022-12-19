19a69c124d18f2384c2ed8ade1e3e485de914d59
Ariel-Santillan <68869573+Ariel-Santillan@users.noreply.github.com>
2022-12-19 17:41:54 -0300

    Agrega entrega ECMAScript

---
+++ ProductManager.js
@@ -0,0 +1,49 @@
+class ProductManager{
+constructor() {
+  this.products = []
+}
+
+
+addProduct = (title, description, price, thumbnail, code, stock) => {
+  if (title && description && price && thumbnail && code && stock){
+    let codeAlreadyExists = this.products.map(prod => prod.code).includes(code)
+    if(!codeAlreadyExists){
+      this.products.push({
+        id: this.products.length,
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock
+      })
+    }else{
+      console.error(`El code ${code} ya existe`)
+    }
+  }else{
+    console.error(`Faltó ingresar algún valor`)
+  }
+}
+
+getProducts = () => {
+  console.log(this.products)
+}
+
+getProductsById(id) {
+  let productFound = this.products.find((prodId) => prodId === id)
+  if(productFound){
+    console.log(productFound)
+  }else{
+    console.error('Not found')
+  }
+}
+
+}
+  
+  
+const productManager = new ProductManager  
+productManager.getProducts()
+productManager.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25)
+productManager.getProducts()
+productManager.addProduct('producto prueba', 'Este es un producto prueba', 200, 'Sin imagen', 'abc123', 25)
+productManager.getProductsById(0)
